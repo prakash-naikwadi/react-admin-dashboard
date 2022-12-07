a7e999f55ec139adafa2f3d241c3c09a99fbd062
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2022-12-07 18:39:51 +0530

    pages added and routes has been set for these pages using react-router-dom, scss added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Single from "./pages/single/Single";
+import List from "./pages/list/List";
+
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="/products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h2 className="h2">child</h2>
+    </div>
+  );
+};
+
+export default Home;
